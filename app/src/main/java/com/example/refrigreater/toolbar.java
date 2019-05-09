7712b824e992634329ea61cfecebf64b9dd2cdaa
scl971009 <dora20823@gmail.com>
2019-05-09 23:16:03 +0800

    first commit (navigation bar)

---
+++ app/src/main/java/com/example/refrigreater/toolbar.java
@@ -0,0 +1,44 @@
+package com.example.refrigreater;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class toolbar extends Activity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_fridge:
+                    mTextMessage.setText(R.string.toolbarfridge);
+                    return true;
+                case R.id.navigation_recipe:
+                    mTextMessage.setText(R.string.toolbarrecipe);
+                    return true;
+                case R.id.navigation_setting:
+                    mTextMessage.setText(R.string.toolbarsetting);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
